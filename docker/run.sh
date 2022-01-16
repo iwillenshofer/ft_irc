034917a341342e65de80ceb2decc8c6da1bd5fa1
iwillens <igor@42sp.org.br>
2022-01-15 22:16:10 -0300

    updated docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 
 docker build --build-arg http_proxy=$http_proxy -t irc-image .
-docker run --rm -t -a stdout -p4443:4443 --name my-irc -v $PWD/ircd.conf:/ircu/lib/ircd.conf:ro irc-image /tmp/run.sh
+docker run --rm -t -a stdout -p4443:4443 --name my-irc irc-image /tmp/run.sh
