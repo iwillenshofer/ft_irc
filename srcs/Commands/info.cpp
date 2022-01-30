0664ead986aa088768d8310e4eefad5a2cf9594a
iwillens <igor@42sp.org.br>
2022-01-30 11:02:36 -0300

    INFO

--- srcs/Commands/info.cpp
+++ srcs/Commands/info.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   info.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roman <roman@student.42.fr>                +#+  +:+       +#+        */
+/*   By: iwillens <iwillens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 19:29:54 by iwillens          #+#    #+#             */
-/*   Updated: 2022/01/27 20:53:01 by roman            ###   ########.fr       */
+/*   Updated: 2022/01/30 10:56:50 by iwillens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,27 @@
 
 void	Commands::_cmd_info(void)
 {
+	std::map<std::string, std::string> m;
+	std::vector<std::string> info;
 
+	if (!(_message.arguments().size())
+	|| (Message::is_bnf_nickname(_message.arguments()[0]) && _get_client_by_nickname(_message.arguments()[0]))
+	|| (Mask::match_raw(_server->servername(), _message.arguments()[0])))
+	{
+		info.push_back("ft_irc: A 42 Network project by @rbittet && @iwillens");
+		info.push_back("Online since: " + ft::format_date(_server->creation_date()));
+		for (std::vector<std::string>::iterator it = info.begin(); it != info.end(); it++)
+		{
+			m["string"] = *it;
+			_message_user(_generate_reply(RPL_INFO, m), _sender);
+		}
+		_message_user(_generate_reply(RPL_ENDOFINFO, m), _sender);
+	}
+	else
+	{
+		m["server name"] = _message.arguments()[0];
+		_message_user(_generate_reply(ERR_NOSUCHSERVER, m), _sender);
+	}
 }
 
 
